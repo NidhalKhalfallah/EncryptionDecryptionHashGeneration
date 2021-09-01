489cd8fa3e91cdb573f32e6c4a3daff0852653e5
NidhalKhalfallah <khalfallahnidhal@live.fr>
2021-09-02 00:21:47 +0200

    Initial WPF Project

---
+++ Views/EncryptDecryptView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EncryptionDecryptionHashGeneration.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour EncryptDecryptView.xaml
+    /// </summary>
+    public partial class EncryptDecryptView : UserControl
+    {
+        public EncryptDecryptView()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            bool? response = openFileDialog.ShowDialog();
+            if(response==true)
+            {
+                MessageBox.Show(openFileDialog.FileName);
+            }
+        }
+    }
+}
