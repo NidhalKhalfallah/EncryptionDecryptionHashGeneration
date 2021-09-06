967fafc38d0eb7463f64a8c202f7e75cd89bb283
NidhalKhalfallah <khalfallahnidhal@live.fr>
2021-09-06 07:04:03 +0200

    MVVM rules implementation

---
+++ ViewModels/EncryptButtonViewModel.cs
@@ -0,0 +1,28 @@
+﻿using EncryptionDecriptionHashGeneration.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptionDecriptionHashGeneration.ViewModels
+{
+    class EncryptButtonViewModel : INotifyPropertyChanged
+    {
+        
+        public EncryptButtonViewModel()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+       
+
+   
+    }
+}
